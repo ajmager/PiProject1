5c5171da9c18ff9e5852135da6e5d34ed59eacc9
ajmager <ajmager@hotmail.ca>
2026-09-09 21:55:16 -0700

    updated image detection, added morphological operation v2

--- src/main.cpp
+++ src/main.cpp
@@ -82,7 +82,7 @@ int main()
         pBackSub->apply(grayFrame, fgMask);                          // apply background subtraction
 
         cv::threshold(fgMask, threshFrame, 200, 255, cv::THRESH_BINARY);
-        cv::dilate(threshFrame, threshFrame, cv::Mat(), cv::Point(-1, -1), 4);
+        cv::dilate(threshFrame, threshFrame, cv::Mat(), cv::Point(-1, -1), 8);
         cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(15, 15));
         cv::morphologyEx(threshFrame, threshFrame, cv::MORPH_CLOSE, kernel);
 
